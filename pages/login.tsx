941f8adea9a1ae0b399c17e5e5515c09ac953b0e
happy8131 <lion8131@naver.com>
2023-07-11 00:28:04 +0900

    useAuth

--- pages/login.tsx
+++ pages/login.tsx
@@ -78,12 +78,19 @@ function Login() {
             )}
           </label>
         </div>
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+        >
           Sign In
         </button>
         <div className="text-[gray]">
           New to Netflix?{" "}
-          <button type="submit" className="text-white hover:underline">
+          <button
+            type="submit"
+            className="text-white hover:underline"
+            onClick={() => setLogin(false)}
+          >
             Sign up now
           </button>
         </div>
