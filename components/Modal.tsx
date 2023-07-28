8ed9b4cc460ffd73e829edd08b7f4b41b8f5761d
happy8131 <lion8131@naver.com>
2023-07-29 00:46:58 +0900

    2:00 ~ 2: 30

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -78,12 +78,9 @@ function Modal() {
             playing
             muted={muted}
           />
-          <div className="absoulte bottom-10 flex w-full items-center justify-between px-10">
+          <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
             <div className="flex space-x-2">
-              <button
-                className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black
-              transition hover:bg-[#e6e6e6]"
-              >
+              <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
                 <FaPlay className="h-7 w-7 text-black" />
                 Play
               </button>
@@ -96,7 +93,52 @@ function Modal() {
                 <ThumbUpIcon className="h-7 w-7" />
               </button>
             </div>
-            <button>{muted ? <VolumeOffIcon /> : <VolumeUpIcon />}</button>
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <VolumeOffIcon className="h-6 w-6" />
+              ) : (
+                <VolumeUpIcon className="h-6 w-6" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10} % Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div
+                className="flex h-4 items-center justify-center rounded border
+              border-white/40 px-1.5 text-xs"
+              >
+                HD
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres.map((genre) => genre.name).join(", ")}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original language: </span>
+                  {movie?.original_language}
+                </div>
+
+                <div className="text-[gray]">
+                  <span className="text-[gray]">Total votes: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
